b65a2880ab5bb820a962cf3592a99d9b5a3fe16c
SOUnit <workbookwork7@gmail.com>
2021-03-08 08:53:51 +0900

    added sidebar

---
+++ src/components/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SideBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type='text' />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
