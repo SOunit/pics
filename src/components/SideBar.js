c66f4b2da666ccd21470f95b740998a379a67708
SOUnit <workbookwork7@gmail.com>
2021-03-08 20:45:36 +0900

    added event handler

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 class SideBar extends React.Component {
+  // naming custom
+  // on + element + action
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className='ui segment'>
         <form className='ui form'>
           <div className='field'>
             <label>Image Search</label>
-            <input type='text' />
+            <input type='text' onChange={this.onInputChange} />
           </div>
         </form>
       </div>
